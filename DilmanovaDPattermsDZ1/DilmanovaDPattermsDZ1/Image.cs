b7666128571fa58801b6da121c483ae427e2933a
Darina <leifmundus@hotmail.com>
2018-01-20 01:53:15 +0600

    mnb

---
+++ DilmanovaDPattermsDZ1/DilmanovaDPattermsDZ1/Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DilmanovaDPattermsDZ1
+{
+  
+    public abstract class Image
+    {
+        string imageName;
+        DateTime imageDate;
+        int imageSize;
+        public Image(string name, DateTime dateTime, int size)
+        {
+            imageName = name;
+            imageDate = dateTime;
+            imageSize = size;
+        }
+        public abstract  Image Clone();
+        }
+   
+}
+

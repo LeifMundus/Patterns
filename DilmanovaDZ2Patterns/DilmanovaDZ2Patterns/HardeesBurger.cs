a6c5906ae497c90d232aa00e6066dcb180727d4a
Darina <leifmundus@hotmail.com>
2018-01-20 03:08:17 +0600

    BurgerMAkerClass fulll

---
+++ DilmanovaDZ2Patterns/DilmanovaDZ2Patterns/HardeesBurger.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DilmanovaDZ2Patterns
+{
+    public class CLient
+    {
+        public void AngusBurgerCombo(HardeesBurgerMaker hbmFacade)
+        {
+            hbmFacade.abcMaker();
+           
+        }
+    }
+    public class HardeesBurgerMaker
+    {
+        BriocheBread briocheBread;
+        CheeseSlices cheeseSlices;
+        BeefBurgerPatty beefBurgerPatty;
+        ComboMaker comboMaker;
+        public HardeesBurgerMaker(BriocheBread brBr, CheeseSlices chSl, BeefBurgerPatty bbp, ComboMaker cm)
+        {
+            briocheBread = brBr;
+            cheeseSlices = chSl;
+            beefBurgerPatty = bbp;
+            comboMaker = cm;
+        }
+        public void abcMaker()
+        {
+            briocheBread.BriocheBreadMaker();
+            Thread.Sleep(1000);
+            cheeseSlices.CheeseSlicesMaker();
+            Thread.Sleep(1000);
+            beefBurgerPatty.BgpMaker();
+            Thread.Sleep(1000);
+            comboMaker.FriesMaker();
+            comboMaker.PourCola();
+            Thread.Sleep(1000);
+            Console.WriteLine("Your Angus Burger Combo is ready! Take it!");
+            Console.ReadLine();
+        }
+    }
+    public class BriocheBread
+    {
+        public void BriocheBreadMaker()
+        {
+            Console.WriteLine("Brioche Bread is ready!");
+            
+            
+        }
+    }
+
+    public class CheeseSlices
+    {
+        public void CheeseSlicesMaker()
+        {
+            Console.WriteLine("Cheese Slices are ready!");
+            
+        }
+    }
+    public class BeefBurgerPatty
+    {
+        public void BgpMaker()
+        {
+            Console.WriteLine("Beef Burger Patty is ready!");
+           
+        }
+    }
+
+    public class ComboMaker
+    {
+        public void PourCola()
+        {
+            Console.WriteLine("CocaCola is ready!");
+           
+        }
+        public void FriesMaker()
+        {
+            Console.WriteLine("Fries is ready!");
+           
+        }
+    }
+    
+        
+    
+}
---
+++ DilmanovaDZ2Patterns/DilmanovaDZ2Patterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DilmanovaDZ2Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BriocheBread briocheBread = new BriocheBread();
+            CheeseSlices cheeseSlices = new CheeseSlices();
+            BeefBurgerPatty beefBurgerPatty = new BeefBurgerPatty();
+            ComboMaker comboMaker = new ComboMaker();
+
+            HardeesBurgerMaker hbm = new HardeesBurgerMaker(briocheBread, cheeseSlices, beefBurgerPatty, comboMaker);
+
+            CLient cLient = new CLient();
+            cLient.AngusBurgerCombo(hbm);
+        }
+    }
+}
